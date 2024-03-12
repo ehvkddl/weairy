d9c5ec40e57bc109bd60068c4009071cfc07ca17
ehvkddl <ehvkddl@naver.com>
2024-03-13 02:44:43 +0900

    rename: 폴더링 #1

--- weairy/weairy/App/weairyApp.swift
+++ weairy/weairy/App/weairyApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct weairyApp: App {
+    @StateObject var container: DIContainer = .init(services: ServiceImpl())
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WeatherView()
+                .environmentObject(container)
         }
     }
 }
