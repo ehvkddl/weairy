affdbe6c8b20c706ba51c4e3ee23b3eba041794a
ehvkddl <ehvkddl@naver.com>
2024-03-13 02:13:27 +0900

    init: Weairy 생성

---
+++ weairy/weairy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weairy
+//
+//  Created by do hee kim on 2024/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weairy/weairy/weairyApp.swift
@@ -0,0 +1,17 @@
+//
+//  weairyApp.swift
+//  weairy
+//
+//  Created by do hee kim on 2024/03/13.
+//
+
+import SwiftUI
+
+@main
+struct weairyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
