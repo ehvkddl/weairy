96963176f5d6a470b431111c4e721b8cc3cdb9bb
ehvkddl <ehvkddl@naver.com>
2024-03-16 21:11:04 +0900

    design: Font 추가 #4

---
+++ weairy/weairy/Extensions/Font+.swift
@@ -0,0 +1,26 @@
+//
+//  Font+.swift
+//  weairy
+//
+//  Created by do hee kim on 2024/03/15.
+//
+
+import SwiftUI
+
+extension Font {
+    // MARK: Lato
+    static let latoLight18: Font = .custom("Lato-Light", size: 18)
+    
+    static let latoRegular18: Font = .custom("Lato-Regular", size: 18)
+    
+    static let latoBold18: Font = .custom("Lato-Bold", size: 18)
+    static let latoBold30: Font = .custom("Lato-Bold", size: 30)
+    
+    // MARK: Poppins
+    static let poppinRegular30: Font = .custom("Poppins-Regular", size: 30)
+    
+    static let poppinMedium22: Font = .custom("Poppins-Medium", size: 22)
+    static let poppinMedium12: Font = .custom("Poppins-Medium", size: 12)
+    
+    static let poppinSemiBold110: Font = .custom("Poppins-SemiBold", size: 110)
+}
